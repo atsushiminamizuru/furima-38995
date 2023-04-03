d2fdc0c60b617781e03bfe0333041d12f75b5be4
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-04-04 02:41:16 +0900

    モデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,8 +1,31 @@
 FactoryBot.define do
+  factory :item_category do
+    id { Faker::Number.between(from: 1, to: 10) }
+  end
+  factory :item_sales_status do
+    id { Faker::Number.between(from: 1, to: 6) }
+  end
+  factory :item_shipping_fee_status do
+    id { Faker::Number.between(from: 1, to: 2) }
+  end
+  factory :item_prefecture do
+    id { Faker::Number.between(from: 1, to: 47) }
+  end
+  factory :item_scheduled_delivery do
+    id { Faker::Number.between(from: 1, to: 3) }
+  end
   factory :item do
-    item_name { "MyString" }
-    item_info { "MyText" }
-    item_price { 1 }
-    user { nil }
+    item_name { Faker::String.random(length: 1..40) }
+    item_info { Faker::Lorem.paragraph_by_chars(number: 1000) }
+    item_price { Faker::Number.between(from: 300, to: 9_999_999) }
+    association :user, factory: :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    association :item_category, factory: :item_category
+    association :item_sales_status, factory: :item_sales_status
+    association :item_shipping_fee_status, factory: :item_shipping_fee_status
+    association :item_prefecture, factory: :item_prefecture
+    association :item_scheduled_delivery, factory: :item_scheduled_delivery
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品登録' do
+    context '新規登録できる場合' do
+      it 'imageとitem_nameとitem_infoとitem_price、item_categoryとitem_sales_status、
+      item_shipping_fee_statusとitem_prefectureとitem_scheduled_deliveryが存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it 'item_priceが半角数値かつ300以上9,999,999以下であれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '新規登録できない場合' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'item_infoが空では登録できない' do
+        @item.item_info = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item info can't be blank")
+      end
+      it 'item_priceが空では登録できない' do
+        @item.item_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item price can't be blank")
+      end
+      it 'item_categoryが空では登録できない' do
+        @item.item_category_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item category can't be blank")
+      end
+      it 'item_sales_statusが空では登録できない' do
+        @item.item_sales_status_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item sales status can't be blank")
+      end
+      it 'item_shipping_fee_statusが空では登録できない' do
+        @item.item_shipping_fee_status_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
+      end
+      it 'item_prefectureが空では登録できない' do
+        @item.item_prefecture_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item prefecture can't be blank")
+      end
+      it 'item_scheduled_deliveryが空では登録できない' do
+        @item.item_scheduled_delivery_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item scheduled delivery can't be blank")
+      end
+      it 'item_priceは半角英字が含まれている場合、登録できない' do
+        @item.item_price = '123a56'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is invalid. Input half-width characters')
+      end
+      it 'item_priceは全角数字を含むと登録できない' do
+        @item.item_price = '123４56'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is invalid. Input half-width characters')
+      end
+      it 'item_priceは全角（漢字・ひらがな・カタカナ）が含まれている場合、登録できない' do
+        @item.item_price = '練しゅウ123'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is invalid. Input half-width characters')
+      end
+      it 'item_priceが299以下では登録できない' do
+        @item.item_price = Faker::Number.between(from: -99_999_999, to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is out of setting range')
+      end
+      it 'item_priceが10,000,000以上では登録できない' do
+        @item.item_price = Faker::Number.between(from: 10_000_000, to: 99_999_999)
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Item price is out of setting range')
+      end
+    end
+  end
 end
