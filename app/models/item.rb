daa362493396da321dd1c99bc6d4446e1dab4a86
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-04-04 02:40:51 +0900

    Rubocop修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,11 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :item_name, presence: true
   validates :item_info, presence: true
-  validates :item_price, presence: true, 
-  numericality: {
-  only_integer: true, message: 'is invalid. Input half-width characters'
-  }, 
-  inclusion: {
-  in: 300..9999999, message: 'is out of setting range'
-  }
+  validates :item_price,
+            presence: true,
+            numericality: { only_integer: true, message: 'is invalid. Input half-width characters' },
+            inclusion: { in: 300..9_999_999, message: 'is out of setting range' }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
   belongs_to :item_sales_status
@@ -18,9 +16,9 @@ class Item < ApplicationRecord
   belongs_to :item_prefecture
   belongs_to :item_scheduled_delivery
 
-  validates :item_category_id, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :item_sales_status_id, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :item_shipping_fee_status_id, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :item_prefecture_id, numericality: { other_than: 0 , message: "can't be blank" }
-  validates :item_scheduled_delivery_id, numericality: { other_than: 0 , message: "can't be blank" }
+  validates :item_category_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :item_sales_status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :item_shipping_fee_status_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :item_prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :item_scheduled_delivery_id, numericality: { other_than: 0, message: "can't be blank" }
 end
