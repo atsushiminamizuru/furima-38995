4377124f2c8ce6b1137b0fa9a3e0f5f7bc757dfe
atsushiminamizuru <utinonekokami19950717@gmail.com>
2023-04-04 19:04:21 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,18 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
